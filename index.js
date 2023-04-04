b0ab5b8ff983c05ad1905bce92a5a8b22aa2d20a
moff <moffmuriithi98@gmail.com>
2023-04-04 12:51:06 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Your code here
+
+const dodger = document.getElementById("dodger");
+
+function moveDodgerLeft() {
+const leftNumbers = dodger.style.left.replace("px", "");
+let left = parseInt(leftNumbers, 10);
+
+if (left > 0) {
+left -= 1;
+dodger.style.left = `${left}px`;
+}
+}
+//move to the right
+function moveDodgerRight() {
+const leftNumbers = dodger.style.left.replace("px", "");
+let left = parseInt(leftNumbers, 10);
+
+if (left < 360) { // Assuming the width of the box is 400px
+left += 1;
+dodger.style.left = `${left}px`;
+}
+}
+
+document.addEventListener("keydown", function (event) {
+if (event.key === "ArrowLeft") {
+moveDodgerLeft();
+} else if (event.key === "ArrowRight") {
+moveDodgerRight();
+}
+});
